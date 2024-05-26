4704fd4478c5d820e51e435d0f1d6957d33a05c4
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-05-26 16:32:13 +0800

    Change Pic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from "../assets/kev.png";
+import logo from "../assets/k.png";
 import {FaLinkedin} from "react-icons/fa"
 import {FaGithub} from "react-icons/fa"
 import {FaFacebook} from "react-icons/fa"
