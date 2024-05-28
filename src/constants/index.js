ff559ff72743eb415fb1db2a429f9df039d06d97
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-05-28 13:51:10 +0800

    Added Experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,7 +14,7 @@ export const EXPERIENCES = [
     role: "Computer Science Student",
     company: "Taguig City University",
     description: `Upcoming 3rd Year Computer Science Students aspiring being a full stack developer`,
-    technologies: ["HTML", "CSS", "Javascript", "PHP", "Java", "Python", "Visual Basic", "MySQL", "Figma", "Git"],
+    technologies: ["HTML", "CSS", "Javascript", "PHP", "Java", "VB.Net", "Python", "MySQL", "Figma"],
   },
   /*{
     year: "2022 - 2023",
