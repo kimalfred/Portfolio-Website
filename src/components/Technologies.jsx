d68360bece1fd52ba591d0e4d471ffe504cea37e
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-05-29 12:52:06 +0800

    Adjust

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -8,53 +8,110 @@ import { SiVisualstudio } from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
 import { FaFigma } from "react-icons/fa6";
 import { FaGitAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
 
+const iconVariants = (duration) => ({
+    initial: { y: -10},
+    animate: {
+        y: [10, -10],
+        transition:{
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        },
+    },
+});
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
         <h2 className="my-20 text-center text-4xl"> Technologies</h2>
         <div className="flex flex-wrap items-center justify-center gap-4">
-            <div className="flex flex-wrap justify-center gap-4">
-                <div className="rounded-2xl border-4 border-neutral-800 p-4 ">
+            <motion.div 
+            whileInView={{ opacity: 1, x: 0}}
+            initial={{ opacity: 0, x:1.5}}
+            transition={{ duration: 1.5}}
+            className="flex flex-wrap justify-center gap-4">
+                <motion.div 
+                variants={iconVariants(2.5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4 ">
                     <FaHtml5 className="text-6xl text-red-500"/>
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div 
+                variants={iconVariants(3)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaCss3Alt className="text-6xl text-blue-500"/>
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div
+                variants={iconVariants(3.5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <RiJavascriptFill className="text-6xl text-yellow-500"/>
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div 
+                variants={iconVariants(4)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaPhp className="text-6xl text-white-800"/>
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div 
+                variants={iconVariants(4.5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaPython className="text-6xl text-yellow-600"/>
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div 
+                variants={iconVariants(5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaJava className="text-6xl text-white-900"/>
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div 
+                variants={iconVariants(5.5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <SiVisualstudio className="text-6xl text-purple-800"/>
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div 
+                variants={iconVariants(6)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <GrMysql className="text-6xl text-blue-600"/>
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div 
+                variants={iconVariants(6.5)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaFigma className="text-6xl text-orange-600"/>
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div 
+                variants={iconVariants(7)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaGitAlt className="text-6xl text-red-700"/>
-                </div>       
-            </div>
+                </motion.div>       
+            </motion.div>
 
         </div>
 
